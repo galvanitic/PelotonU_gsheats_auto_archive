53ec8e301631626b60d9e50c9765712423b3c0e3
galvanitic <develop@rglvn.com>
2022-05-31 10:15:51 -0500

    Logic is all in place. Next we need to automate and deploy.

--- peloton.py
+++ peloton.py
@@ -1,63 +1,46 @@
-from __future__ import print_function
+from datetime import date
+import pygsheets
 
-import os.path
+# Global Constants
+HCN_ONEGOAL_SHEET_ID = '1CaaeH03NZOxjQVwQZDusU89Uw_-nqi3RkLJFElziwnk'
+HCN_ONEGOAL_SHEET_ID_TEST = '11iHlbJxX_UxAZDxhmHl-0pqnSKq26xbjIS4lC3gW02I'
+ONEGOAL_ARCHIVE_FOLDER_ID = '1ocgNSorHOp045yh1XbqIzZ2ALmf65Fvd'
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
-SAMPLE_RANGE_NAME = 'Class Data!A2:E'
+# Authorize and create instance
+gc = pygsheets.authorize(local=True)
 
+def createBackup():
+    time_of_backup = date.today().strftime("%m/%d/%Y")
+    new_sheet_name = "Archive created: "+time_of_backup
+    gc.drive.copy_file(HCN_ONEGOAL_SHEET_ID, new_sheet_name, ONEGOAL_ARCHIVE_FOLDER_ID);
+    print('New backup created at '+time_of_backup)
+
+def clearTemplate(sheet_id):
+    '''
+    Clears all unnecessary data from sheet.
+    :param sheet_id: The ID of the sheet to clear.
+    :return: Void
+    '''
+    sheet_to_clear = gc.open_by_key(sheet_id)
+
+    # Clear 'Dashboard'
+    Dashboard = sheet_to_clear.worksheet(property='index', value=0)
+    Dashboard.clear(start='C3', end='H10')
+    Dashboard.clear(start='C14', end='H18')
+
+    # Clear 'Detailed Academic Data'
+    Detailed_Academic_Data = sheet_to_clear.worksheet(property='index', value=1)
+    Detailed_Academic_Data.clear(start='A2', end=None)
+
+    # Clear 'Detailed Recruitment Data'
+    Detailed_Recruitment_Data = sheet_to_clear.worksheet(property='index', value=2)
+    Detailed_Recruitment_Data.clear(start='A2', end=None)
 
 def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=8080)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    try:
-        service = build('sheets', 'v4', credentials=creds)
-
-        # Call the Sheets API
-        sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                    range=SAMPLE_RANGE_NAME).execute()
-        values = result.get('values', [])
-
-        if not values:
-            print('No data found.')
-            return
-
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[4]))
-    except HttpError as err:
-        print(err)
-
+    # createBackup()
+    clearTemplate(HCN_ONEGOAL_SHEET_ID_TEST)
 
 if __name__ == '__main__':
     main()
