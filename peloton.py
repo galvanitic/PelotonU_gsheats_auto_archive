bc2dfc0a9f5586cf2c4142385d6f5ac7a06e00dc
galvanitic <develop@rglvn.com>
2022-06-13 23:44:44 -0500

    Adjusted some constants for the object-based implementation.

--- peloton.py
+++ peloton.py
@@ -1,21 +1,19 @@
 from datetime import date
 import pygsheets
 
-# Global Constants
-HCN_ONEGOAL_SHEET_ID = '1CaaeH03NZOxjQVwQZDusU89Uw_-nqi3RkLJFElziwnk'
-HCN_ONEGOAL_SHEET_ID_TEST = '1ER4WlZ9vMVOYW904DgCvThjr-3IedW4AGHJ9_cwIkvY'
-ONEGOAL_ARCHIVE_FOLDER_ID = '1ocgNSorHOp045yh1XbqIzZ2ALmf65Fvd'
-
 class HCNSheetsDriver:
-    def __init__(self):
+    def __init__(self, main_sheet_id, archive_folder_id):
         # Authorize and create instance
         self.gc = pygsheets.authorize(local=True)
+        # Global Constants
+        self.HCN_ONEGOAL_SHEET_ID = main_sheet_id
+        self.ONEGOAL_ARCHIVE_FOLDER_ID = archive_folder_id
 
 
     def createBackup(self):
         time_of_backup = date.today().strftime("%m/%d/%Y")
         new_sheet_name = "Archive created: "+time_of_backup
-        self.gc.drive.copy_file(HCN_ONEGOAL_SHEET_ID, new_sheet_name, ONEGOAL_ARCHIVE_FOLDER_ID);
+        self.gc.drive.copy_file(self.HCN_ONEGOAL_SHEET_ID, new_sheet_name, self.ONEGOAL_ARCHIVE_FOLDER_ID);
         print('New backup created at '+time_of_backup)
 
     def clearTemplate(self, sheet_id):
@@ -40,9 +38,10 @@ class HCNSheetsDriver:
         Detailed_Recruitment_Data.clear(start='A2', end=None)
 
 def main():
-    hcn = HCNSheetsDriver()
+    hcn = HCNSheetsDriver('1CaaeH03NZOxjQVwQZDusU89Uw_-nqi3RkLJFElziwnk', '1ocgNSorHOp045yh1XbqIzZ2ALmf65Fvd')
+    
     # hcn.createBackup()
-    hcn.clearTemplate(HCN_ONEGOAL_SHEET_ID_TEST)
+    hcn.clearTemplate('1ER4WlZ9vMVOYW904DgCvThjr-3IedW4AGHJ9_cwIkvY')
 
 if __name__ == '__main__':
     main()
